f3d06adb1b1cd4ebace7c49b2297714722435e09
marcodev1x <contatomarcodev@gmail.com>
2025-08-27 16:23:24 -0300

    fix: removed merge conflict content.

--- src/domains/gymnia-config-params/services.ts
+++ src/domains/gymnia-config-params/services.ts
@@ -9,7 +9,6 @@ export class GymniaConfigParamsService {
         return await this.gymniaConfigParamsRepository.getConfigParams();
     }
 
-<<<<<<< HEAD
     async getSpecificConfigParam(param: GymniaConfigParamsEnum): Promise<GymniaConfigParams> {
         const configParam = await this.gymniaConfigParamsRepository.getSpecificConfigParam(param);
 
@@ -19,15 +18,4 @@ export class GymniaConfigParamsService {
 
         return configParam;
     }
-=======
-  async getSpecificConfigParam(param: GymniaConfigParamsEnum): Promise<GymniaConfigParams> {
-    const configParam = await this.gymniaConfigParamsRepository.getSpecificConfigParam(param);
-
-    if (!configParam) {
-      throw ThrowHttpError({ element: 'ConfigParam', error: 'NOT_FOUND' });
-    }
-
-    return configParam;
-  }
->>>>>>> 9b6933fc3715e65ee699f096c9ad92d2dac2a4d0
 }
