3571382d6c2c09414b1ce846a4e6d1d399652f26
marcodev1x <contatomarcodev@gmail.com>
2025-09-23 20:40:45 -0300

    feat: added final feat to better see the server running in dev

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ import logger from './logger';
 
 app.listen(
     appConfig.port, () => {
-        if (isDevelopment) logger.info(`Server running on port ${appConfig.port}`);
+        if (isDevelopment) {
+            const developmentMessages = [
+                { message: `Server running in ${appConfig.port} port` },
+                { message: `Environment: ${appConfig.local}` },
+                { message: `Process ID: ${process.pid}` },
+            ];
+
+            developmentMessages.map(logging => logger.info(logging.message));
+        } else {
+            return;
+        }
     },
 );
