c3f880960b4555ee8a985a279a20087824c21fb1
Marco Antonio <167551490+marcodev1x@users.noreply.github.com>
2025-10-13 14:14:55 -0300

    Fix build reqs (#23)
    * fix: fixed build with request pag.
    
    * fix: fixed build with request pag.
    
    * fix: fixed build with request pag.
    
    * fix: fix unused vars.

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,11 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Request } from 'express';
 
 declare module 'express-serve-static-core' {
     interface Request {
       pagination?: {
-        page: number;
-        limit: number;
-        skip: number;
+        page?: number;
+        limit?: number;
+        offset?: number;
       };
     }
   }
