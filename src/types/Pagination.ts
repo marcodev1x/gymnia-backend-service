97369214248eb9f8a6299a7e6830a3a65001c502
Marco Antonio <167551490+marcodev1x@users.noreply.github.com>
2025-10-13 14:19:06 -0300

    Fix build try again (#24)
    * fix: fixed build with request pag.
    
    * fix: fixed build with request pag.
    
    * fix: fixed build with request pag.
    
    * fix: fix unused vars.
    
    * fix: fixed erros.

--- src/types/Pagination.ts
+++
@@ -1,5 +0,0 @@
-export type Pagination = {
-    page: number;
-    limit: number;
-    offset: number;
-};
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,12 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Request } from 'express';
 
+export type Pagination = {
+    page: number;
+    limit: number;
+    offset: number;
+};
+
 declare module 'express-serve-static-core' {
     interface Request {
-      pagination?: {
-        page?: number;
-        limit?: number;
-        offset?: number;
-      };
+      pagination?: Pagination;
     }
   }
